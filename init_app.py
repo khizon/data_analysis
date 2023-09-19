341a8219a9b557aa1a7f03db4a65ccd753d9211b
Kiel <kielhizon@gmail.com>
2023-09-19 14:38:56 +0800

    Initial Commit

---
+++ init_app.py
@@ -0,0 +1,17 @@
+## This code is used to check imports
+import numpy as np
+import pandas as pd
+import pandas_profiling
+import prettypandas
+import dabl
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import spacy
+import sklearn
+import xgboost as xgb
+
+from tqdm import tqdm
+import os
+
+print(f'Import Successful')
